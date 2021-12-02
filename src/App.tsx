7212cc08173e086d9a33dee3012170e134c7a837
Diego Isaac Herrera Ledezma <dhlisaac@gmail.com>
2021-12-01 18:25:15 -0600

    npm package fixes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
 
-export default function Home() {
+export default function MyTable():any {
   return (
     <div>
       <p>Componente de tabla</p>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import MyTable from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+export const Table = MyTable;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
